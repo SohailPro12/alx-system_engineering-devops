5dc1dd61a3eea850b0b6299f080a6a7f9574a9d2
Sohail Charef <charefsohayl@gmail.com>
2024-01-19 11:06:04 -0800

    read me

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
 # install flask from pip3.
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
