ead9b5d659b8d9303e5eb9146caf0eec1f6a16b3
Sohail Charef <charefsohayl@gmail.com>
2024-01-20 06:52:06 -0800

    conf

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# config ssh
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
