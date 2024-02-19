7859c4d62ef90b0bfe407f18f527813a0460e9ae
SohailPro12 <131702341+SohailPro12@users.noreply.github.com>
2024-02-19 22:24:03 +0100

    emp

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     params = {"userId": emp_id}
     todos = requests.get(url + "todos", params).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [todo.get("title") for todo in todos if todo.get("completed") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
