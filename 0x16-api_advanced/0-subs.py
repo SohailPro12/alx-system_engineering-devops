2abf2497c8c7936e0199f5f7fd7c987ae0192fac
Sohail Charef <charefsohayl@gmail.com>
2024-03-05 03:39:31 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    function that return the number of subscribers
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'A user agent 1.0'}
     response = requests.get(url, headers=headers)
