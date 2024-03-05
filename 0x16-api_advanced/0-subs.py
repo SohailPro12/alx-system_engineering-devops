a64f9363c1efa204b4a117a1831a633b5545ee28
Sohail Charef <charefsohayl@gmail.com>
2024-03-05 09:38:25 -0800

    up

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,9 @@ def number_of_subscribers(subreddit):
     function that return the number of subscribers
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Curious_Salt3631)"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Curious_Salt3631)"
+    }
     response = requests.get(url, headers=headers,
                             allow_redirects=False)
     if response.status_code == 200:
